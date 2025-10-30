0b15f0b3c0c71deb058795335efb1f9230f4b687
Alexander Ulrich <alexander.ulrich@ymail.com>
2025-10-30 17:00:13 +0100

    added response for invalid http requests, updated test requests

--- src/routers/orderRouter.ts
+++ src/routers/orderRouter.ts
@@ -10,10 +10,32 @@ import { Router } from "express";
 
 const orderRouter = Router();
 
+//Valid Routes
 orderRouter.get("/", getAllOrders);
 orderRouter.post("/", validateBodyZod(orderInputSchema), createOrder);
 
 orderRouter.get("/:id", getOrderById);
 orderRouter.delete("/:id", deleteOrder);
+
+//Method not allowed
+orderRouter.put("/", (req, res) =>
+  res.status(405).json({ message: "Error: 405 Method not allowed!" })
+);
+orderRouter.patch("/", (req, res) =>
+  res.status(405).json({ message: "Error: 405 Method not allowed!" })
+);
+orderRouter.delete("/", (req, res) =>
+  res.status(405).json({ message: "Error: 405 Method not allowed!" })
+);
+
+orderRouter.post("/:id", (req, res) =>
+  res.status(405).json({ message: "Error: 405 Method not allowed!" })
+);
+orderRouter.put("/:id", (req, res) =>
+  res.status(405).json({ message: "Error: 405 Method not allowed!" })
+);
+orderRouter.patch("/:id", (req, res) =>
+  res.status(405).json({ message: "Error: 405 Method not allowed!" })
+);
 
 export default orderRouter;
